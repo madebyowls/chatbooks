5de87d22b6a0e4fc6b053d6d08491ff8953abad8
Nick Christensen <nick.g.christensen@gmail.com>
2014-01-31 14:47:53 -0700

    removed jquery dep

--- js/chatbooks.js
+++ js/chatbooks.js
@@ -1,15 +1,42 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(){
+	var fader = document.querySelector('.floater .fade');
+	var header = document.querySelector('header');
+	window.addEventListener('scroll', function(){
+		var breakpoint = header.offsetHeight;
+		var hasClass = classTest(fader, inClass);
 
-	// Toggling the header download links
-	var $fader = $('.floater .fade');
-	var header = $('header')[0];
-	$(window).on('scroll', function(){
-		var breakpoint = header.scrollHeight;
-		if( window.scrollY > breakpoint && !$fader.hasClass('in') )
-			$fader.addClass('in');
+		if( window.scrollY > breakpoint && !hasClass ){
+			if (fader.classList){
+				fader.classList.add(inClass);
+			}
+			else {
+				fader.className += ' ' + inClass;
+			}
+		}
 
-		else if( window.scrollY <= breakpoint && $fader.hasClass('in') )
-			$fader.removeClass('in');
-
+		else if( window.scrollY <= breakpoint && hasClass){
+			if (fader.classList){
+				fader.classList.remove(inClass);
+			}
+			else {
+				fader.className = fader.className.replace(new RegExp('(^| )' + inClass.split(' ').join('|') + '( |$)', 'gi'), ' ');
+			}
+		}
 	});
 });
+
+var inClass ="in";
+
+
+function classToggler(){
+}
+
+function classTest(el, className){
+	if (el.classList)
+		if (el.classList.contains(className))
+			return true;
+	else
+		if( new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className) )
+			return true;
+	return false;
+}
