84ea6a6111378b0afbb630d0f42fcb3c18c37ddd
Nick Christensen <nick.g.christensen@gmail.com>
2014-02-02 14:01:42 -0700

    Changed method of getting scroll position for IE, sytax error in classTest function

--- js/chatbooks.js
+++ js/chatbooks.js
@@ -1,11 +1,13 @@
+var doc = document.documentElement, body = document.body;
+var inClass ="in";
 document.addEventListener('DOMContentLoaded', function(){
 	var fader = document.querySelector('.floater .fade');
 	var header = document.querySelector('header');
 	window.addEventListener('scroll', function(){
+		var top = (doc && doc.scrollTop  || body && body.scrollTop  || 0);
 		var breakpoint = header.offsetHeight;
 		var hasClass = classTest(fader, inClass);
-
-		if( window.scrollY > breakpoint && !hasClass ){
+		if( top > breakpoint && !hasClass ){
 			if (fader.classList){
 				fader.classList.add(inClass);
 			}
@@ -14,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function(){
 			}
 		}
 
-		else if( window.scrollY <= breakpoint && hasClass){
+		else if( top <= breakpoint && hasClass){
 			if (fader.classList){
 				fader.classList.remove(inClass);
 			}
@@ -25,18 +27,14 @@ document.addEventListener('DOMContentLoaded', function(){
 	});
 });
 
-var inClass ="in";
-
-
-function classToggler(){
-}
-
 function classTest(el, className){
-	if (el.classList)
+	if (el.classList){
 		if (el.classList.contains(className))
 			return true;
-	else
+	}
+	else{
 		if( new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className) )
 			return true;
+	}
 	return false;
 }
